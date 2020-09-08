9091ff462a143ae6f3693e65fac72ccc7501fc18
sh4869 <nobuk4869@gmail.com>
2020-09-08 20:48:07 +0900

    20200908_190244(OK)
    - server setup

--- src/main/scala/fpms/Main.scala
+++ src/main/scala/fpms/Main.scala
@@ -8,16 +8,42 @@ import scala.util.control.Breaks
 import scala.util.Try
 import com.github.sh4869.semver_parser.{Range, SemVer}
 import cats.effect.{IO, IOApp, ExitCode}
+import org.http4s.server.blaze.BlazeServerBuilder
+import org.http4s.HttpRoutes
 import cats.implicits._
 
 object Fpms extends IOApp {
   private val logger = LoggerFactory.getLogger(this.getClass)
 
+  def helloWorldService(map: Map[Int, PackageNode]) = {
+    import io.circe.generic.auto._
+    import io.circe.syntax._
+    import org.http4s.circe._
+    import org.http4s.dsl.io._
+    import org.http4s.implicits._
+    import org.http4s.server.blaze._
+    implicit val userDecoder = jsonEncoderOf[IO, Option[PackageNode]]
+    HttpRoutes
+      .of[IO] {
+        case GET -> Root / "hello" / name =>
+          Ok(s"Hello, $name.")
+        case GET -> Root / "id" / IntVar(id) =>
+          Ok(map.get(id))
+      }
+      .orNotFound
+  }
+
   def run(args: List[String]): IO[ExitCode] = {
     logger.info("setup")
     val map = setup()
     algo(map)
-    IO.unit.as(ExitCode.Success)
+    BlazeServerBuilder[IO]
+      .bindHttp(8080, "localhost")
+      .withHttpApp(helloWorldService(map))
+      .serve
+      .compile
+      .drain
+      .as(ExitCode.Success)
   }
 
   def setup(): Map[Int, PackageNode] = {

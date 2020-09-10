234c4633e952ca7e525b18e9a7f82446825be5f4
sh4869 <nobuk4869@gmail.com>
2020-09-10 23:32:45 +0900

    サーバーモードの実装

--- src/main/scala/fpms/Main.scala
+++ src/main/scala/fpms/Main.scala
@@ -13,29 +13,46 @@ import org.http4s.HttpRoutes
 import cats.implicits._
 import com.typesafe.config._
 import doobie._
+import fpms.repository.SourcePackageRepository
+import fpms.repository.db.SourcePackageSqlRepository
+import cats.data.NonEmptyList
 
 object Fpms extends IOApp {
   private val logger = LoggerFactory.getLogger(this.getClass)
 
-  def helloWorldService(map: Map[Int, PackageNode]) = {
+  def convertToResponse(repo: SourcePackageRepository[IO], node: PackageNode): PackageNodeRespose = {
+    val result = for {
+      src <- repo.findById(node.src)
+      directed <- repo.findByIds(node.directed.toList.toNel.get)
+      set <- repo.findByIds(node.packages.toList.toNel.get)
+    } yield PackageNodeRespose(src.get, directed, set.toSet)
+    result.unsafeRunSync()
+  }
+
+  def helloWorldService(map: Map[Int, PackageNode], repo: SourcePackageRepository[IO]) = {
     import io.circe.generic.auto._
     import io.circe.syntax._
     import org.http4s.circe._
     import org.http4s.dsl.io._
     import org.http4s.implicits._
-    import org.http4s.server.blaze._
-    implicit val userDecoder = jsonEncoderOf[IO, Option[PackageNode]]
+    implicit val userDecoder = jsonEncoderOf[IO, PackageNodeRespose]
     HttpRoutes
       .of[IO] {
         case GET -> Root / "hello" / name =>
           Ok(s"Hello, $name.")
         case GET -> Root / "id" / IntVar(id) =>
-          Ok(map.get(id))
+          Ok(convertToResponse(repo, map.get(id).get))
       }
       .orNotFound
   }
 
-  def saveToDb() {
+  def saveToDb(xa: Transactor[IO]) {
+    val packs = JsonLoader.loadIdList()
+    util.SqlSaver.saveJson(packs, xa)
+  }
+
+  def run(args: List[String]): IO[ExitCode] = {
+
     val config = ConfigFactory.load("app.conf").getConfig("server.postgresql")
     val xa = Transactor.fromDriverManager[IO](
       config.getString("driver"),
@@ -43,20 +60,16 @@ object Fpms extends IOApp {
       config.getString("user"),
       config.getString("pass")
     )
-    val packs = JsonLoader.loadIdList()
-    util.SqlSaver.saveJson(packs, xa)
-  }
-
-  def run(args: List[String]): IO[ExitCode] = {
+    val repo = new SourcePackageSqlRepository[IO](xa)
     if (args.headOption.exists(_ == "db")) {
-      saveToDb()
+      saveToDb(xa)
     }
     logger.info("setup")
     val map = setup()
     algo(map)
     BlazeServerBuilder[IO]
       .bindHttp(8080, "localhost")
-      .withHttpApp(helloWorldService(map))
+      .withHttpApp(helloWorldService(map, repo))
       .serve
       .compile
       .drain
@@ -208,5 +221,10 @@ object Fpms extends IOApp {
       src: Int,
       directed: Seq[Int],
       packages: scala.collection.mutable.Set[Int]
+  )
+  case class PackageNodeRespose(
+      src: SourcePackage,
+      directed: Seq[SourcePackage],
+      packages: Set[SourcePackage]
   )
 }

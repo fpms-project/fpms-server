25427ef2b3574d759eef3595a697a59391c133de
sh4869 <nobuk4869@gmail.com>
2020-12-03 19:50:05 +0900

    SqlSaverの高速化

--- src/main/scala/fpms/util/SqlSaver.scala
+++ src/main/scala/fpms/util/SqlSaver.scala
@@ -10,13 +10,20 @@ import fpms.json.RootInterfaceN
 import fpms.repository.db.LibraryPackageSqlRepository
 
 object SqlSaver extends LazyLogging {
-  def saveJson(packages: Array[RootInterfaceN], repo: LibraryPackageSqlRepository[IO]) = {
-    for (i <- 0 to packages.length - 1) {
-      if (i % 10000 == 0) logger.info(s"save in db: ${i + 1}/${packages.length - 1}")
-      val pack = packages(i)
-      val list =
-        pack.versions.map(x => Try { Some(LibraryPackage(pack.name, x.version, x.dep, x.id)) }.getOrElse(None)).flatten
-      repo.insert(list.toList).unsafeRunSync()
+  val GROUPED = 100
+  def saveJson(packages: List[RootInterfaceN], repo: LibraryPackageSqlRepository[IO]) = {
+    packages.grouped(GROUPED).zipWithIndex.foreach {
+      case (v, i) => {
+        if ((i * GROUPED) % 10000 == 0) logger.info(s"save in db: ${i * GROUPED}/${packages.length}")
+        val list = v
+          .map(pack =>
+            pack.versions
+              .map(x => Try { Some(LibraryPackage(pack.name, x.version, x.dep, x.id)) }.getOrElse(None))
+              .flatten
+          )
+          .flatten
+        repo.insert(list.toList).unsafeRunSync()
+      }
     }
   }
 }

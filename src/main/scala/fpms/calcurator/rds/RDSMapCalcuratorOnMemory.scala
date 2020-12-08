7ec32222151d43baf6c3a852134ee5c657ce718c
sh4869 <nobuk4869@gmail.com>
2020-12-09 04:08:46 +0900

    Revert "groupedでやってみたが、これもうまく行ってない"
    This reverts commit 2fead8cf4e12c35451b6587f2444c210603f86a8.

--- src/main/scala/fpms/calcurator/rds/RDSMapCalcuratorOnMemory.scala
+++ src/main/scala/fpms/calcurator/rds/RDSMapCalcuratorOnMemory.scala
@@ -10,7 +10,6 @@ import com.typesafe.scalalogging.LazyLogging
 import fpms.calcurator.ldil.LDILMap
 import cats.effect.ConcurrentEffect
 import cats.effect.concurrent.Semaphore
-import cats.syntax.group
 
 class RDSMapCalcuratorOnMemory[F[_]](implicit F: ConcurrentEffect[F], P: Parallel[F])
     extends RDSMapCalcurator[F]
@@ -19,50 +18,55 @@ class RDSMapCalcuratorOnMemory[F[_]](implicit F: ConcurrentEffect[F], P: Paralle
   def calc(ldilMap: LDILMap): F[RDSMap[F]] = {
     val initedMap = initMap(ldilMap)
     val allMap = initedMap._1
+    val allMapList = allMap.toList
     var updated = initedMap._2
-    val grouped = 10000
-    val allMapList = allMap.toList.grouped(grouped).zipWithIndex
+    // Loop
+    val semaphor = F.toIO(Semaphore.apply(16)).unsafeRunSync()
     while (updated.nonEmpty) {
       logger.info(s"updated size: ${updated.size}")
       val updateInLoop = F.toIO(MVar.of[F, Set[Int]](Set.empty[Int])).unsafeRunSync()
+      val lock = F.toIO(MVar.of[F, Unit](())).unsafeRunSync()
+      F.toIO(lock.take).unsafeRunSync()
+      val count = F.toIO(MVar.of[F, Int](0)).unsafeRunSync()
       val checkFunction: (Int => Boolean) =
         if (updated.size / ldilMap.size > 0.5) { (_) => true }
         else updated.contains
       // 最初からSemaphoreでやってくれるようなやつを作る必要がある
-      allMapList.foreach {
-        case (list, i) => {
-          if (i * grouped % 1000000 == 0) logger.info(s"${i * grouped}")
-          F.toIO(list.map {
-              case (id, setMvar) => {
-                for {
-                  set <- setMvar.read
-                  newSet <- MVar.of[F, Set[Int]](set)
-                  _ <- {
-                    ldilMap.get(id).fold(F.pure(())) { value =>
-                      value.map { tid =>
-                        if (checkFunction(tid)) {
-                          for {
-                            x <- allMap.get(tid).get.read
-                            z <- newSet.take
-                            _ <- newSet.put(x ++ z)
-                          } yield ()
-                        } else F.unit
-                      }.parSequence.void
-                    }
-                  }
-                  x <- newSet.read
-                  _ <- if (x.size > set.size) {
-                    for {
-                      _ <- updateInLoop.take.flatMap(list => updateInLoop.put(list + id))
-                      _ <- setMvar.swap(x)
-                    } yield ()
-                  } else F.pure(())
-                } yield ()
+      allMapList.map {
+        case (id, setMvar) =>
+          F.toIO(for {
+              _ <- semaphor.acquire
+              set <- setMvar.read
+              newSet <- MVar.of[F, Set[Int]](set)
+              _ <- {
+                ldilMap.get(id).fold(F.pure(())) { value =>
+                  value.map { tid =>
+                    if (checkFunction(tid)) {
+                      for {
+                        x <- allMap.get(tid).get.read
+                        z <- newSet.take
+                        _ <- newSet.put(x ++ z)
+                      } yield ()
+                    } else F.unit
+                  }.parSequence.void
+                }
               }
-            }.toNel.get.parSequence_)
-            .unsafeRunSync()
-        }
+              x <- newSet.read
+              _ <- if (x.size > set.size) {
+                for {
+                  _ <- updateInLoop.take.flatMap(list => updateInLoop.put(list + id))
+                  _ <- setMvar.swap(x)
+                } yield ()
+              } else F.pure(())
+              x <- count.take
+              _ <- if (x + 1 >= allMap.size) lock.put(()) else F.pure(())
+              _ <- count.put(x + 1)
+              _ <- semaphor.release
+            } yield ())
+            .unsafeRunAsyncAndForget()
       }
+      // lockから取れるようになるまで待つ
+      F.toIO(lock.take).unsafeRunSync()
       updated = F.toIO(updateInLoop.read).unsafeRunSync()
     }
     F.pure(allMap)

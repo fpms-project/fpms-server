76d2289472ba85e53fa523bdae2d961151d1b780
sh4869 <nobuk4869@gmail.com>
2020-12-14 00:50:23 +0900

    メモリ使用量の問題を解決

--- src/main/scala/fpms/calcurator/ldil/LDILMapCalculatorOnMemory.scala
+++ src/main/scala/fpms/calcurator/ldil/LDILMapCalculatorOnMemory.scala
@@ -1,15 +1,22 @@
 package fpms.calcurator.ldil
 
+import java.util.concurrent.Executors
+
+import scala.concurrent.ExecutionContext
+
+import cats.Parallel
+import cats.effect.ContextShift
+import cats.implicits._
+import com.typesafe.scalalogging.LazyLogging
+
 import fpms.LibraryPackage
 import fpms.json.JsonLoader
-import com.typesafe.scalalogging.LazyLogging
-import cats.effect.Async
-import cats.Parallel
-import cats.implicits._
+import cats.effect.ConcurrentEffect
 
-class LDILMapCalculatorOnMemory[F[_]](implicit F: Async[F], P: Parallel[F])
+class LDILMapCalculatorOnMemory[F[_]](implicit F: ConcurrentEffect[F], P: Parallel[F], cs: ContextShift[F])
     extends LDILMapCalculator[F]
     with LazyLogging {
+
   private val added = scala.collection.mutable.ListBuffer.empty[LibraryPackage]
   def init: F[LDILMap] = {
     updateMap(JsonLoader.createNamePackagesMap())
@@ -28,24 +35,29 @@ class LDILMapCalculatorOnMemory[F[_]](implicit F: Async[F], P: Parallel[F])
     val packsGroupedByName = packMap.values.toList.map(_.toList).grouped(packMap.size / 16).zipWithIndex.toList
     val finder = new LatestDependencyFinder(packMap.get)
     logger.info(s"number of names of packages : ${packMap.size}")
-    packsGroupedByName.map {
-      case (list, i) => {
-        F.async[Map[Int, List[Int]]](cb => {
-          val map = scala.collection.mutable.Map.empty[Int, List[Int]]
-          list.foreach { v =>
-            v.foreach { pack =>
-              try {
-                val ids = finder.findIds(pack)
-                map.update(pack.id, ids)
-              } catch {
-                case _: Throwable => ()
+    val executor = Executors.newFixedThreadPool(16)
+    val context = ExecutionContext.fromExecutor(executor)
+    val x = F
+      .toIO(cs.evalOn(context)(packsGroupedByName.map {
+        case (list, i) => {
+          F.async[Map[Int, List[Int]]](cb => {
+            val map = scala.collection.mutable.Map.empty[Int, List[Int]]
+            list.foreach { v =>
+              v.foreach { pack =>
+                try {
+                  val ids = finder.findIds(pack)
+                  map.update(pack.id, ids)
+                } catch {
+                  case _: Throwable => ()
+                }
               }
             }
-          }
-          logger.info(s"end $i thread")
-          cb(Right(map.toMap))
-        })
-      }
-    }.toList.parSequence.map(_.flatten.toMap)
+            logger.info(s"end $i thread")
+            cb(Right(map.toMap))
+          })
+        }
+      }.toList.parSequence.map(_.flatten.toMap)))
+      .unsafeRunSync()
+    F.pure(x)
   }
 }

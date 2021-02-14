96410284d64d222ea519f78f5af5cb29b47db69f
sh4869 <nobuk4869@gmail.com>
2021-02-14 17:02:37 +0900

    update package.sql

--- sql/package.sql
+++ sql/package.sql
@@ -2,7 +2,6 @@ CREATE TABLE package (
     name VARCHAR(255) NOT NULL,
     version VARCHAR(255) NOT NULL,
     deps JSON,
-    deps_latest JSON,
     id integer NOT NULL,
     UNIQUE(name, version),
     PRIMARY KEY (id)

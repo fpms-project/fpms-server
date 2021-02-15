0c58f798a5df8a44fb2851c4f492c154e46ab3f2
sh4869 <nobuk4869@gmail.com>
2021-02-15 20:44:34 +0900

    save RDS as a string
    not occured oom killer, but slove(5m -> 15m)

--- common/src/main/scala/fpms/RDS.scala
+++ common/src/main/scala/fpms/RDS.scala
@@ -1,5 +1,9 @@
 package fpms
 
+case class RDS(src: String) {
+  def to: Set[Int] = src.split(",").map(_.toInt).toSet
+}
 object RDS {
-  type RDSMap = Map[Int, scala.collection.Set[Int]]
+  def apply(s: Set[Int]): RDS = RDS(s.mkString(","))
+  type RDSMap = Map[Int, RDS]
 }

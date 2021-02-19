feec99b5c3c56824ea56216936711fe2121228f5
sh4869 <nobuk4869@gmail.com>
2021-02-20 03:45:27 +0900

    fix apis

--- server/src/main/scala/fpms/server/ServerApp.scala
+++ server/src/main/scala/fpms/server/ServerApp.scala
@@ -8,8 +8,8 @@ import com.typesafe.scalalogging.LazyLogging
 import io.circe.generic.auto._
 import org.http4s.HttpApp
 import org.http4s.HttpRoutes
-import org.http4s.circe._
 import org.http4s.dsl._
+import org.http4s.dsl.impl.OptionalQueryParamDecoderMatcher
 import org.http4s.implicits._
 
 import fpms.LibraryPackage
@@ -28,22 +28,24 @@ class ServerApp[F[_]](
 ) extends LazyLogging {
   object dsl extends Http4sDsl[F]
 
-  def convertToResponse(target: Int, allSet: Set[Int]): F[PackageNodeRespose] =
+  def convertToResponse(target: Int, allSet: Set[Int]): F[PackageRDS] =
+    // TODO: validation(targetにdepsが指定されていてallSetがzeroだった場合はエラーにする)
     for {
       src <- packageRepo.findOne(target)
       set <- packageRepo.findByIds(allSet.toList)
-    } yield PackageNodeRespose(src.get, set.toSet)
-  def getPackages(name: String, range: String): F[Either[String, PackageNodeRespose]] = {
-    logger.info(s"start get package from redis: ${name}@${range}")
+    } yield PackageRDS(src.get, set.toSet)
+
+  def getPackages(request: PackageRequest): F[Either[String, PackageRDS]] = {
+    logger.info(s"start get package from redis: ${request.name}@${request.range}")
     (for {
       // Rangeのパース
-      range <- EitherT.fromOption[F](Range.parse(range), "cannot parse the range")
+      range <- EitherT.fromOption[F](Range.parse(request.range), "cannot parse the range")
       // name パッケージを探す
       packs <- EitherT(
-        packageRepo.findByName(name).map {
+        packageRepo.findByName(request.name).map {
           _ match {
             case head :: tl => Right(head :: tl)
-            case Nil        => Left(s"package ${name} is not found")
+            case Nil        => Left(s"package ${request.name} is not found")
           }
         }
       )
@@ -53,7 +55,7 @@ class ServerApp[F[_]](
         "No packages were found that met the version requirements"
       )
       // 計算結果とレスポンスの取得
-      res <- EitherT.fromOptionF[F, String, PackageNodeRespose](
+      res <- EitherT.fromOptionF[F, String, PackageRDS](
         for {
           v <- rdsRepository.get(target.id)
           x <- convertToResponse(target.id, v.getOrElse(Set.empty)).map(Some(_))
@@ -72,22 +74,34 @@ class ServerApp[F[_]](
       _ <- addedQueue.push(id)
     } yield ()
 
+  private def getLatestRange(name: String) =
+    packageRepo
+      .findByName(name)
+      .map(ps => {
+        val z = ps
+          .filter(p => p.version.build.isEmpty && p.version.preRelease.isEmpty)
+          .sortWith((l, r) => l.version > r.version)
+          .head
+          .version
+          .original
+        "^" + z
+      })
+
   def app(): HttpApp[F] = {
     import dsl._
-    // ここどうにかならないか
     implicit val libraryEncoder = LibraryPackage.encoder
-    implicit val encoder = jsonEncoderOf[F, PackageNodeRespose]
-    implicit val encoder2 = jsonEncoderOf[F, List[LibraryPackage]]
+    import org.http4s.circe.CirceEntityEncoder._
     implicit val libraryDecoder = LibraryPackage.decoder
-    implicit val x = jsonOf[F, LibraryPackage]
-    // ここどうにかならないか
+    import org.http4s.circe.CirceEntityDecoder._
+
     HttpRoutes
       .of[F] {
-        case GET -> Root / "get_package" / name =>
+        case GET -> Root / "packages" / name =>
           for {
             list <- packageRepo.findByName(name)
             x <- Ok(list)
           } yield x
+
         case GET -> Root / "id" / IntVar(id) =>
           for {
             v <- rdsRepository.get(id)
@@ -96,13 +110,19 @@ class ServerApp[F[_]](
               case None        => NotFound()
             }
           } yield x
-        case GET -> Root / "get" / name / range =>
-          getPackages(name, range).flatMap(_ match {
-            case Right(v) => Ok(v)
-            case Left(v)  => NotFound(v)
-          })
+
+        case GET -> Root / "calculated" / name :? RangeQueryParamMatcher(r) =>
+          for {
+            range <- if (r.isDefined) F.pure(r.get) else getLatestRange(name)
+            request = PackageRequest(name, range)
+            res <- getPackages(request).flatMap(_ match {
+              case Right(v) => Ok(PackageRDSResponse(request, v))
+              case Left(v)  => NotFound(v)
+            })
+          } yield res
+
         case GET -> Root / "getyarn" / name / range =>
-          getPackages(name, range).flatMap(_ match {
+          getPackages(PackageRequest(name, range)).flatMap(_ match {
             case Right(value) => Ok(YarnLockGenerator.generateYarn(value.packages + value.target, (name, range)))
             case Left(value)  => NotFound(value)
           })
@@ -117,7 +137,16 @@ class ServerApp[F[_]](
   }
 }
 
-case class PackageNodeRespose(
+case class PackageRequest(name: String, range: String)
+
+case class PackageRDS(
     target: LibraryPackage,
     packages: Set[LibraryPackage]
 )
+
+case class PackageRDSResponse(
+    request: PackageRequest,
+    rds: PackageRDS
+)
+
+object RangeQueryParamMatcher extends OptionalQueryParamDecoderMatcher[String]("range")

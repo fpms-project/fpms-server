b3bc786dc689d974de296cbf911d1667153313ec
sh4869 <nobuk4869@gmail.com>
2021-04-29 14:56:20 +0900

    RDSを削除

--- calculator/src/main/scala/fpms/calculator/rds/RDSMapCalculatorOnMemory.scala
+++ calculator/src/main/scala/fpms/calculator/rds/RDSMapCalculatorOnMemory.scala
@@ -11,7 +11,6 @@ import cats.implicits._
 import com.typesafe.scalalogging.LazyLogging
 
 import fpms.LDIL.LDILMap
-import fpms.RDS
 import fpms.RDS.RDSMap
 
 class RDSMapCalculatorOnMemory[F[_]](implicit F: ConcurrentEffect[F], P: Parallel[F], cs: ContextShift[F])
@@ -21,9 +20,9 @@ class RDSMapCalculatorOnMemory[F[_]](implicit F: ConcurrentEffect[F], P: Paralle
   def calc(ldilMap: LDILMap): F[RDSMap] = {
     logger.info("start calculation of RDS")
     val (allMap, updatedZ) = initMap(ldilMap)
-    val keyGrouped = allMap.keySet.grouped(allMap.size / 15).toList
+    val keyGrouped = allMap.keySet.grouped(allMap.size / 31).toList
     var updatedBefore = updatedZ
-    val context = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(16))
+    val context = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(32))
     logger.info(s"created initalized map - updated size: ${updatedBefore.size}")
     // Loop
     while (updatedBefore.nonEmpty) {
@@ -36,12 +35,12 @@ class RDSMapCalculatorOnMemory[F[_]](implicit F: ConcurrentEffect[F], P: Paralle
                 // 前回アップデートされたidそれぞれについてそれぞれの依存関係集合を取得
                 val d = ldil.filter(updatedBefore.contains)
                 if (d.nonEmpty) {
-                  val list = allMap.get(rdsid).get.to
-                  val depsList = d.map(tid => allMap.get(tid).map(_.to)).flatten.flatten
+                  val list = allMap.get(rdsid).get
+                  val depsList = d.map(tid => allMap.get(tid)).flatten.flatten
                   val newList = (depsList ++ list).distinct
                   if (newList.size > list.size) {
                     update += rdsid
-                    allMap.update(rdsid, RDS(newList.toArray))
+                    allMap.update(rdsid, newList.toArray)
                   }
                 }
               }
@@ -57,13 +56,13 @@ class RDSMapCalculatorOnMemory[F[_]](implicit F: ConcurrentEffect[F], P: Paralle
     F.pure(allMap.toMap)
   }
 
-  private def initMap(ldilMap: LDILMap): (scala.collection.mutable.Map[Int, RDS], Set[Int]) = {
-    val allMap = scala.collection.mutable.LinkedHashMap.empty[Int, RDS]
+  private def initMap(ldilMap: LDILMap): (scala.collection.mutable.Map[Int, Array[Int]], Set[Int]) = {
+    val allMap = scala.collection.mutable.LinkedHashMap.empty[Int, Array[Int]]
     val updatedIni = scala.collection.mutable.Set.empty[Int]
     ldilMap.toList.map {
       case (id, set) => {
         if (set.nonEmpty) {
-          allMap.update(id, RDS(set.toArray))
+          allMap.update(id, set.toArray)
           updatedIni += id
         }
       }
--- common/src/main/scala/fpms/RDS.scala
+++ common/src/main/scala/fpms/RDS.scala
@@ -1,21 +1,5 @@
 package fpms
 
-case class RDS(src: Either[String, Array[Int]]) {
-  def toStr: String = src match {
-    case Left(v)  => v
-    case Right(v) => v.mkString(",")
-  }
-
-  def to: Array[Int] = src match {
-    case Right(v)  => v
-    case Left(str) => str.split(",").map(_.toInt)
-  }
-}
-
 object RDS {
-  // どのぐらいの長さからStringに変換するか
-  private val LENGTH_FOR_CONVERTING_TO_STRING = 900
-  def apply(s: Array[Int]): RDS =
-    if (s.size > LENGTH_FOR_CONVERTING_TO_STRING) RDS(Left(s.mkString(","))) else RDS(Right(s))
-  type RDSMap = Map[Int, RDS]
+  type RDSMap = Map[Int, Array[Int]]
 }

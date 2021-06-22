4c331e1eb07c0316891400ad3a8922c629aed6af
sh4869 <nobuk4869@gmail.com>
2021-06-22 15:17:23 +0900

    update ROOT response

--- server/src/main/scala/fpms/server/ServerApp.scala
+++ server/src/main/scala/fpms/server/ServerApp.scala
@@ -16,7 +16,6 @@ import fpms.LibraryPackage
 import fpms.repository.AddedPackageIdQueue
 import fpms.repository.LibraryPackageRepository
 import fpms.repository.RDSRepository
-import org.http4s.headers.Location
 
 class ServerApp[F[_]](
     packageRepo: LibraryPackageRepository[F],
@@ -120,8 +119,8 @@ class ServerApp[F[_]](
             })
           } yield res
 
-        case GET -> Root => 
-          TemporaryRedirect(Location(uri"https://github.com/sh4869/fpms-server"))
+        case GET -> Root =>
+          Ok(ServerApp.ROOT_DESCRIPTION)
 
         case req @ POST -> Root / "add" =>
           for {
@@ -146,3 +145,77 @@ case class PackageRDSResponse(
 )
 
 object RangeQueryParamMatcher extends OptionalQueryParamDecoderMatcher[String]("range")
+
+object ServerApp {
+  val ROOT_DESCRIPTION =
+    """# API
+
+- HOST: `http://fpms-server.cs.ise.shibaura-it.ac.jp`
+
+## `/calculated/$name`
+
+returns the calculated `$name` package.
+
+### optional query
+
+- `range`: version range, see also [semver.npmjs.com](https://semver.npmjs.com/)
+
+### response example
+
+```json
+{
+  "target": {
+    "name": "react",
+    "version": "17.0.1",
+    "dep": {
+      "loose-envify": "^1.1.0",
+      "object-assign": "^4.1.1"
+    },
+    "shasum": "6e0600416bd57574e3f86d92edba3d9008726127",
+    "integrity": "sha512-lG9c9UuMHdcAexXtigOZLX8exLWkW0Ku29qPRU8uhF2R9BN96dLCt0psvzPLlHc5OWkgymP3qwTRgbnw5BKx3w=="
+  },
+  "packages": [
+    {
+      "name": "object-assign",
+      "version": "4.1.1",
+      "dep": {},
+      "shasum": "2109adc7965887cfc05cbbd442cac8bfbb360863",
+      "integrity": ""
+    },
+    {
+      "name": "loose-envify",
+      "version": "1.4.0",
+      "dep": {
+        "js-tokens": "^3.0.0 || ^4.0.0"
+      },
+      "shasum": "71ee51fa7be4caec1a63839f7e682d8132d30caf",
+      "integrity": "sha512-lyuxPGr/Wfhrlem2CL/UcnUc1zcqKAImBDzukY7Y5F/yQiNdko6+fRLevlw1HgMySw7f611UIY408EtxRSoK3Q=="
+    },
+    {
+      "name": "js-tokens",
+      "version": "4.0.0",
+      "dep": {},
+      "shasum": "19203fb59991df98e3a287050d4647cdeaf32499",
+      "integrity": "sha512-RdJUflcE3cUzKiMqQgsCu06FPu9UdIJO0beYbPhHN4k6apgJtifcoCtT9bcxOpYBtpD2kCM6Sbzg4CausW/PKQ=="
+    }
+  ]
+}
+```
+
+## `POST /add`
+
+added packages to the server.
+
+### body format
+
+```
+{
+  "id": 0, // always 0
+  "name": "package_name", // package name 
+  "version": "0.0.0", // semantic version
+  "deps": {}, // deps of packages
+  "shasum": "" // shasum of package. It can get in npm or yarn api.
+}
+```
+"""
+}

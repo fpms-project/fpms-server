e7a80847f26b413b1969f682290c61cf662b5c62
sh4869 <nobuk4869@gmail.com>
2021-10-12 16:52:20 +0900

    add parallelazation but it doesnt work

--- calculator/src/main/scala/fpms/calculator/rds/OrderComputeRDSMapCalculator.scala
+++ calculator/src/main/scala/fpms/calculator/rds/OrderComputeRDSMapCalculator.scala
@@ -1,20 +1,49 @@
 package fpms.calculator.rds
 
+import java.util.concurrent.Executors
+
+import scala.concurrent.ExecutionContext
+
+import cats.Parallel
 import cats.effect.ConcurrentEffect
+import cats.effect.ContextShift
+import cats.implicits._
 import com.typesafe.scalalogging.LazyLogging
 
 import fpms.LDIL
 import fpms.RDS
 
-class OrderComputeRDSMapCalculator[F[_]](implicit F: ConcurrentEffect[F])
-    extends RDSMapCalculator[F] with LazyLogging {
+// 並列化するとどこかでデッドロックになって動かないっぽい。
+// 本質的な話でもないのでしばらく放置しておく
+class OrderComputeRDSMapCalculator[F[_]](implicit F: ConcurrentEffect[F], P: Parallel[F], cs: ContextShift[F])
+    extends RDSMapCalculator[F]
+    with LazyLogging {
+
+  lazy val THREAD_NUM = Runtime.getRuntime().availableProcessors() / 2
+  lazy val context = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(THREAD_NUM))
+
   def calc(ldilMap: LDIL.LDILMap): F[RDS.RDSMap] = {
+    logger.info("start compute rds")
     val allMap = scala.collection.mutable.LinkedHashMap.empty[Int, Array[Int]]
-    ldilMap.zipWithIndex.foreach(v => {
-      if(v._2 % 1000000 == 0) logger.info(s"compute ${v._2} / ${ldilMap.size}")
-      val rds = calcRDS(v._1._1, ldilMap, allMap)
-      allMap.addOne((v._1._1, rds))
-    })
+    val grouped = ldilMap.keys.grouped(ldilMap.size / Math.max(THREAD_NUM - 1, 1)).zipWithIndex.toList
+    val func = grouped
+      .map(group => {
+        logger.info(s"start computes in thread ${group._2}")
+        F.async[Unit](cb => {
+          var count = 0
+          group._1.foreach(v => {
+            if (count % 100 == 0) logger.info(s"compute ${count} th on thread ${group._2}")
+            val rds = calcRDS(v, ldilMap, allMap)
+            allMap.addOne((v, rds))
+            count += 1
+          })
+          logger.info(s"end thread ${group._2}")
+          cb(Right(()))
+        })
+      })
+      .toList
+      .parSequence_
+    F.toIO(cs.evalOn(context)(func)).unsafeRunSync()
     F.pure(allMap.toMap)
   }
 
@@ -23,11 +52,12 @@ class OrderComputeRDSMapCalculator[F[_]](implicit F: ConcurrentEffect[F])
     val request = scala.collection.mutable.HashSet(ldilMap.get(id).getOrElse(Seq.empty): _*)
     val requested = scala.collection.mutable.HashSet.empty[Int]
     while (request.size > 0) {
-      request.toSeq.foreach((id: Int) => {
+      val thisList = request.toSeq
+      thisList.foreach((id: Int) => {
         requested.add(id)
         if (allMap.get(id).isDefined) {
           set.addAll(allMap.get(id).get)
-        } else if(ldilMap.get(id).isDefined) {
+        } else if (ldilMap.get(id).isDefined) {
           val ldil = ldilMap.get(id).get
           set.addAll(ldil)
           request.addAll(ldil.filterNot(requested.contains))

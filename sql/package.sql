476611b7df52d84abc04e768cc5ccc59c221d84d
sh4869 <nobuk4869@gmail.com>
2021-11-13 14:54:40 +0900

    update postgres

--- sql/package.sql
+++ sql/package.sql
@@ -2,7 +2,7 @@ CREATE TABLE package (
     name VARCHAR(255) NOT NULL,
     version VARCHAR(255) NOT NULL,
     deps JSON,
-    id integer NOT NULL,
+    id SERIAL NOT NULL,
     shasum VARCHAR(255) NOT NULL,
     integrity VARCHAR(255),
     UNIQUE(name, version),
